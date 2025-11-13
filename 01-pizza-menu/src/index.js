329ad630f52de62bdf689056afccf5c85a469034
ripperd-2882 <ripperd2882@gmail.com>
2025-11-13 18:34:33 +0530

    Fix typo in closing hour display in Order component

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -123,7 +123,7 @@ function Order(props) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHour}::00. Come visit us or order online
+        We're open until {props.closeHour}:00. Come visit us or order online
       </p>
       <button className="btn">Order</button>
     </div>
