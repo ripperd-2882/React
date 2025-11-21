46a57646438d14d6d82de8d9e4c5617da96b66ba
ripperd-2882 <ripperd2882@gmail.com>
2025-11-21 19:57:35 +0530

    Add FormSplitBill component for bill splitting functionality

--- 04-eat-n-split/src/App.js
+++ 04-eat-n-split/src/App.js
@@ -1,5 +1,3 @@
-import { Children } from "react";
-
 const initialFriends = [
   {
     id: 118836,
@@ -29,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -80,6 +79,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💵 Bill Value</label>
+      <input type="text" />
+
+      <label>🙍 Your expense</label>
+      <input type="text" />
+
+      <label>👯 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
