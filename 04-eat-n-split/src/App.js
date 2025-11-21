8dea23e7034dd0fea91c3d0fcc716c827f2864da
ripperd-2882 <ripperd2882@gmail.com>
2025-11-21 19:43:44 +0530

    Refactor App component to include FormAddFriend and Button components; replace button elements with reusable Button component.

--- 04-eat-n-split/src/App.js
+++ 04-eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { Children } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -24,6 +26,8 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
     </div>
   );
@@ -57,7 +61,25 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👯 Friend's Name</label>
+      <input type="text" />
+
+      <label>📷 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
   );
 }
