a92f80378c29f05b294f6db25ec0b3875cc03fac
ripperd-2882 <ripperd2882@gmail.com>
2025-11-23 02:41:31 +0530

    Refactor StarRating component to manage rating state and update display; clean up unused SVG code in index.css

--- 05-usepopcorn/src/index.js
+++ 05-usepopcorn/src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
