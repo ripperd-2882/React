eb570490338984f844d5b9882d478c61273d65fd
ripperd-2882 <ripperd2882@gmail.com>
2025-11-26 16:24:23 +0530

    Add triple increment functionality to TabContent component

--- 06-how-react-works/src/App.js
+++ 06-how-react-works/src/App.js
@@ -71,6 +71,16 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  function handleTripleInc() {
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
   function handleUndo() {
     setShowDetails(true);
     setLikes(0);
@@ -90,7 +100,7 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
