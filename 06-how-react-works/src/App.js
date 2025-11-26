08ba7bd780f8b0a3094a25444a4ea69241f24e2d
ripperd-2882 <ripperd2882@gmail.com>
2025-11-26 16:27:37 +0530

    Add delayed undo functionality to TabContent component

--- 06-how-react-works/src/App.js
+++ 06-how-react-works/src/App.js
@@ -88,6 +88,10 @@ function TabContent({ item }) {
     console.log(likes);
   }
 
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -107,7 +111,7 @@ function TabContent({ item }) {
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
