7e4159748fec6f813e7c07fe98d86234b9f95d8b
ripperd-2882 <ripperd2882@gmail.com>
2025-11-30 01:50:24 +0530

    Refactor layout in index.css for flexible box sizing; update index.js to enable main app rendering

--- 05-usepopcorn/src/index.js
+++ 05-usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
@@ -17,13 +17,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       className=""
       message={["poor", "bad", "fair", "good", "excellent"]}
     />
     <StarRating color="blue" size={124} defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
