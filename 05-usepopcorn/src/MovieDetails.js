0b1f26e4119509ea41adc7528185a59c57aacdc8
ripperd-2882 <ripperd2882@gmail.com>
2025-12-09 22:27:26 +0530

    demo- useRef variables are persisted across the renders

--- 05-usepopcorn/src/MovieDetails.js
+++ 05-usepopcorn/src/MovieDetails.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Loader } from "./Loader";
 import StarRating from "./StarRating";
 
@@ -11,6 +11,15 @@ export function MovieDetails({
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current++;
+    },
+    [userRating]
+  );
 
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
@@ -39,6 +48,7 @@ export function MovieDetails({
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRef,
     };
 
     onAddWatched(newWatchedMovie);
