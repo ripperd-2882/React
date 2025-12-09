b5d9b5f6539a6b10ceccca0b719204ef78586b68
ripperd-2882 <ripperd2882@gmail.com>
2025-12-09 23:04:43 +0530

    removed callback- causing infinite loop

--- 05-usepopcorn/src/useMovies.js
+++ 05-usepopcorn/src/useMovies.js
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
 
-export function useMovies(query, callback) {
+export function useMovies(query) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(
     function () {
-      callback?.();
-
       const controller = new AbortController();
 
       async function fetchMovies() {
