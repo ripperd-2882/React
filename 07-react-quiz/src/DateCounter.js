0da271114f7a98088d24c184cebd1ad18adb01b2
ripperd-2882 <ripperd2882@gmail.com>
2025-12-13 23:47:38 +0530

    back to the correct way of reseting the data

--- 07-react-quiz/src/DateCounter.js
+++ 07-react-quiz/src/DateCounter.js
@@ -1,5 +1,7 @@
 import { useReducer, useState } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
   switch (action.type) {
@@ -12,11 +14,12 @@ function reducer(state, action) {
     case "setStep":
       return { ...state, step: action.payload };
     case "reset":
-      return {
-        ...state,
-        count: action.payload.count,
-        step: action.payload.step,
-      };
+      return initialState;
+    // return {
+    //   ...state,
+    //   count: action.payload.count,
+    //   step: action.payload.step,
+    // };
     // return { count: 0, state: 1 };
     default:
       throw new Error("Unknown action");
@@ -29,7 +32,6 @@ function reducer(state, action) {
 function DateCounter() {
   // const [count, setCount] = useState(0);
   // const [step, setStep] = useState(1);
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
