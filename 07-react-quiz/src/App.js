374f99eb88f6c00101e4ccaa982e59e94478b11f
ripperd-2882 <ripperd2882@gmail.com>
2025-12-15 02:56:18 +0530

    used useReducer for initialState and questions

--- 07-react-quiz/src/App.js
+++ 07-react-quiz/src/App.js
@@ -1,13 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   });
 
   return (
