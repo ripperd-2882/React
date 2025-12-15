e3ae18ee7d0150349321a2574e9f61a5fea3a637
ripperd-2882 <ripperd2882@gmail.com>
2025-12-16 02:53:28 +0530

    questions and options appear on the screen

--- 07-react-quiz/src/App.js
+++ 07-react-quiz/src/App.js
@@ -8,8 +8,8 @@ import Question from "./Question";
 
 const initialState = {
   questions: [],
-
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -39,7 +39,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -60,7 +63,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
