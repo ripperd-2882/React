43ab64f551387a145acae63e3c4503ff59f3d9e2
ripperd-2882 <ripperd2882@gmail.com>
2025-12-17 03:25:17 +0530

    implemented FinishScreen

--- 07-react-quiz/src/components/App.js
+++ 07-react-quiz/src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -54,6 +55,12 @@ function reducer(state, action) {
         ...state,
         index: state.index + 1,
         answer: null,
+      };
+
+    case "quizFinished":
+      return {
+        ...state,
+        status: "finished",
       };
 
     default:
@@ -104,8 +111,16 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
         )}
       </Main>
     </div>
---
+++ 07-react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,20 @@
+function FinishScreen({ points, maxPossiblePoints }) {
+  const percentage = Math.ceil((points / maxPossiblePoints) * 100);
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😄";
+  if (percentage > 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "😔";
+
+  return (
+    <p className="result">
+      <span>{emoji}</span>
+      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+      {percentage}) %
+    </p>
+  );
+}
+
+export default FinishScreen;
