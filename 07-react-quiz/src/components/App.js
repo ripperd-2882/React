092652994d9881682e3d9faff05e6aafea38a280
ripperd-2882 <ripperd2882@gmail.com>
2025-12-16 22:48:32 +0530

    calculating the points

--- 07-react-quiz/src/components/App.js
+++ 07-react-quiz/src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -35,9 +36,15 @@ function reducer(state, action) {
       };
 
     case "newAnswer":
+      const question = state.questions.at(state.index);
+
       return {
         ...state,
         answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
       };
 
     default:
