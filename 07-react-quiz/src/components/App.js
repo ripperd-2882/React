050fc6beb117388c036c9cedf548395080caba4c
ripperd-2882 <ripperd2882@gmail.com>
2025-12-16 22:41:05 +0530

    selecting the correct option

--- 07-react-quiz/src/components/App.js
+++ 07-react-quiz/src/components/App.js
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   status: "loading",
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -33,13 +34,19 @@ function reducer(state, action) {
         status: "active",
       };
 
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
+
     default:
       throw new Error("Action is unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -63,7 +70,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
