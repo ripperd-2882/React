b384a5ea223312da7db31b4b97a3550a60808900
ripperd-2882 <ripperd2882@gmail.com>
2025-12-21 02:33:13 +0530

    feat: integrate Button component into Form; update Map to navigate to Form on click

--- 08-worldwise/src/App.jsx
+++ 08-worldwise/src/App.jsx
@@ -9,6 +9,7 @@ import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -52,7 +53,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
