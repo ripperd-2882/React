5fb0389ee8d458e9b7f16edf3e551090289f4ad9
ripperd-2882 <ripperd2882@gmail.com>
2025-12-21 02:41:33 +0530

    fix: ensure proper navigation replacement to cities on index route

--- 08-worldwise/src/App.jsx
+++ 08-worldwise/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to="cities" />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
