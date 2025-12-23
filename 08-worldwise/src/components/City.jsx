5edceaf39aba67cf35fb251826cda109d91e878d
ripperd-2882 <ripperd2882@gmail.com>
2025-12-24 05:13:45 +0530

    feat: update City component to conditionally render flag emoji and enhance Map component with dynamic center change

--- 08-worldwise/src/components/City.jsx
+++ 08-worldwise/src/components/City.jsx
@@ -42,7 +42,7 @@ function City() {
       <div className={styles.row}>
         <h6>City name</h6>
         <h3>
-          <span>{flagEmojiToPNG(emoji)}</span> {cityName}
+          <span>{emoji && flagEmojiToPNG(emoji)}</span> {cityName}
         </h3>
       </div>
 
--- 08-worldwise/src/components/Map.jsx
+++ 08-worldwise/src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 const flagEmojiToPNG = (flag) => {
@@ -19,15 +19,22 @@ function Map() {
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -46,9 +53,17 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
