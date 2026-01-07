096701e0d1468f69ff389824bb17cb9ba24f0d4b
ripperd-2882 <ripperd2882@gmail.com>
2026-01-08 01:01:21 +0530

    feat: conditionally render position button based on geolocation availability

--- 08-worldwise/src/components/Map.jsx
+++ 08-worldwise/src/components/Map.jsx
@@ -52,9 +52,11 @@ function Map() {
 
   return (
     <div className={styles.mapContainer}>
-      <Button type="position" onClick={getPosition}>
-        {isLoadingPosition ? "Loading..." : "Use your position"}
-      </Button>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
