d963bab298d94bf4169a82f49fedff7e8c2b1a68
ripperd-2882 <ripperd2882@gmail.com>
2026-01-10 14:34:43 +0530

    feat: refactor flag emoji rendering to use country code for image source

--- 08-worldwise/src/components/CityItem.jsx
+++ 08-worldwise/src/components/CityItem.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import { useCities } from "../contexts/CitiesContext";
 
-const flagEmojiToPNG = (flag) => {
-  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-    .join("");
+const flagCodeToPNG = (countryCode) => {
+  if (!countryCode) return null;
+
   return (
-    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+    <img
+      src={`https://flagcdn.com/24x18/${countryCode.toLowerCase()}.png`}
+      alt="flag"
+    />
   );
 };
 
@@ -30,7 +32,7 @@ function CityItem({ city }) {
         }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
-        <span className={styles.emoji}>{flagEmojiToPNG(emoji)}</span>
+        <span className={styles.emoji}>{flagCodeToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
         <button className={styles.deleteBtn}>&times;</button>
