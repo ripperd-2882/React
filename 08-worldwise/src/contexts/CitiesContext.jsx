505a4ae8006e9a946b6b9803b81375d73ab1f926
ripperd-2882 <ripperd2882@gmail.com>
2026-01-11 13:01:01 +0530

    feat: update reducer to manage currentCity state on city creation and deletion

--- 08-worldwise/src/contexts/CitiesContext.jsx
+++ 08-worldwise/src/contexts/CitiesContext.jsx
@@ -35,6 +35,7 @@ function reducer(state, action) {
         ...state,
         isLoading: false,
         cities: [...state.cities, action.payload],
+        currentCity: action.payload,
       };
 
     case "city/deleted":
@@ -42,6 +43,7 @@ function reducer(state, action) {
         ...state,
         isLoading: false,
         cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
       };
 
     case "rejected":
@@ -57,7 +59,7 @@ function reducer(state, action) {
 }
 
 function CitiesProvider({ children }) {
-  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -87,6 +89,8 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
+
     dispatch({ type: "loading" });
 
     try {
@@ -153,6 +157,7 @@ function CitiesProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        error,
         getCity,
         createCity,
         deleteCity,
