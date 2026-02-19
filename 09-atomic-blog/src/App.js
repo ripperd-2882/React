46d7ef60ea548e19d137d8269cee692e5ad11aa8
ripperd-2882 <ripperd2882@gmail.com>
2026-02-20 01:12:12 +0530

    refactor: reorganize App component structure and revert App import to original

--- 09-atomic-blog/src/App.js
+++ 09-atomic-blog/src/App.js
@@ -21,21 +21,21 @@ function App() {
   );
 
   return (
-    <PostProvider>
-      <section>
-        <button
-          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-          className="btn-fake-dark-mode"
-        >
-          {isFakeDark ? "☀️" : "🌙"}
-        </button>
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
 
+      <PostProvider>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </section>
-    </PostProvider>
+      </PostProvider>
+    </section>
   );
 }
 
