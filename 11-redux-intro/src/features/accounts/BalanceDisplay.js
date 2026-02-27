8ecfde466c455ca38cc6055875f5ed8bd48fd7a3
ripperd-2882 <ripperd2882@gmail.com>
2026-02-28 02:38:59 +0530

    feat: connect BalanceDisplay component to Redux store for balance management - the old way of connecting redux

--- 11-redux-intro/src/features/accounts/BalanceDisplay.js
+++ 11-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { connect, useSelector } from "react-redux";
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
@@ -7,9 +7,15 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  const balance = useSelector((state) => state.account.balance);
+function BalanceDisplay({ balance }) {
+  // const balance = useSelector((state) => state.account.balance);
   return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
