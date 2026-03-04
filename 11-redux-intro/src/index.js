66ac95781e5d9c96284cfeb90bc94c1e667d0358
ripperd-2882 <ripperd2882@gmail.com>
2026-03-05 01:53:56 +0530

    feat: enhance AccountOperations with currency conversion and loading state management

--- 11-redux-intro/src/index.js
+++ 11-redux-intro/src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
+// store.dispatch({ type: "account/deposit", payload: 250 });
 // console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
