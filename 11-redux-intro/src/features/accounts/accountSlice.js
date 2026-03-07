3a818c431e7fd18ed18c2af24b70cd53336727e1
ripperd-2882 <ripperd2882@gmail.com>
2026-03-08 02:31:12 +0530

    feat: refactor requestLoan reducer to use prepare callback for improved action structure

--- 11-redux-intro/src/features/accounts/accountSlice.js
+++ 11-redux-intro/src/features/accounts/accountSlice.js
@@ -17,17 +17,24 @@ const accountSlice = createSlice({
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
 
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance = state.balance + action.payload.amount;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
     },
     payLoan(state, action) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.purpose = "";
-      state.balance -= state.loan;
     },
   },
 });
