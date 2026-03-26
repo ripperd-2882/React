dc27e7b59bd3bf234aa616e76bf8356c4a21b462
ripperd-2882 <ripperd2882@gmail.com>
2026-03-26 23:19:10 +0530

    added border, margins and padding

--- 12-fast-react-pizza/src/ui/Header.jsx
+++ 12-fast-react-pizza/src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase tracking-widest">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase tracking-widest">
       <Link to="/">Fast React Pizza Co.</Link>
 
       <SearchOrder />
