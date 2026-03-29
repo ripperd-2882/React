1909a8b0fc41eb13add16407601c2474f7df4c99
ripperd-2882 <ripperd2882@gmail.com>
2026-03-29 17:38:12 +0530

    implement add to cart functionality in MenuItem and update Button component to support onClick prop

--- 12-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 12-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,9 +1,24 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from './../../utils/helpers';
 import Cart from './../cart/Cart';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: 1 * unitPrice,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -27,7 +42,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to Cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to Cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
