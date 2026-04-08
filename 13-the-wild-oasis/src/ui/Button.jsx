1e532e2b30d584f7615569b819b7a7c290046f4b
ripperd-2882 <ripperd2882@gmail.com>
2026-04-09 02:40:17 +0530

    Refactor App component to use Heading component; fix border-radius variable in Button

--- 13-the-wild-oasis/src/ui/Button.jsx
+++ 13-the-wild-oasis/src/ui/Button.jsx
@@ -53,7 +53,7 @@ const Button = styled.button`
   padding: 1.2rem 1.6rem;
   font-weight: 500;
   border: none;
-  border-radius: var() (--border-radius-sm);
+  border-radius: var(--border-radius-sm);
   background-color: var(--color-brand-600);
   color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
