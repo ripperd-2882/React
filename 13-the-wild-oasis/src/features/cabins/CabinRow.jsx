1da614134c654efd2100e3d7df4264a360c1bf1e
ripperd-2882 <ripperd2882@gmail.com>
2026-05-19 00:45:46 +0530

    Implement ConfirmDelete modal in CabinRow for enhanced delete confirmation; refactor button handling

--- 13-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 13-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -5,6 +5,7 @@ import { useDeleteCabin } from "./useDeleteCabin";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
+import ConfirmDelete from "./../../ui/ConfirmDelete";
 
 const TableRow = styled.div`
   display: grid;
@@ -97,9 +98,19 @@ function CabinRow({ cabin }) {
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
 
-          <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-            <HiTrash />
-          </button>
+          <Modal.Open>
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
+
+          <Modal.Window>
+            <ConfirmDelete
+              resourceName="cabins"
+              disabled={isDeleting}
+              onConfirm={() => deleteCabin(cabinId)}
+            />
+          </Modal.Window>
         </Modal>
       </div>
     </TableRow>
