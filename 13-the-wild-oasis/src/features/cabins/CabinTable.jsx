e3d6be15409396f9e8f773f3a50b99cb0a10bbc3
ripperd-2882 <ripperd2882@gmail.com>
2026-05-21 02:24:31 +0530

    Refactor CabinTable to utilize Table.Body for rendering cabins; improve data handling and add empty state message

--- 13-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 13-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -33,9 +33,11 @@ function CabinTable() {
         <div>Discount</div>
         <div></div>
       </Table.Header>
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
     </Table>
   );
 }
