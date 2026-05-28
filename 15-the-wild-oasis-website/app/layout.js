8c97c023415153edbbb145d9e59980af20a3c8a2
ripperd-2882 <ripperd2882@gmail.com>
2026-05-29 02:10:59 +0530

    Add custom font styling and update body class in layout

--- 15-the-wild-oasis-website/app/layout.js
+++ 15-the-wild-oasis-website/app/layout.js
@@ -1,6 +1,13 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josafin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
@@ -17,7 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josafin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
