bd9c8ead407dfcc6dd72a336f1f00121e4e51360
ripperd-2882 <ripperd2882@gmail.com>
2026-05-30 16:55:47 +0530

    Refactor CabinCard component to use Next.js Image component for optimized image loading; update Page component to fetch cabins data asynchronously and adjust metadata; configure Next.js for remote image patterns

--- 15-the-wild-oasis-website/app/cabins/page.js
+++ 15-the-wild-oasis-website/app/cabins/page.js
@@ -1,12 +1,13 @@
 import CabinCard from "../_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
@@ -17,9 +18,9 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
       {cabins.length > 0 && (
--- 15-the-wild-oasis-website/next.config.mjs
+++ 15-the-wild-oasis-website/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bmddbsxwhbkvcgsqzoss.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 export default nextConfig;
