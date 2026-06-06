2d2db09e15637bb1b5f855bf8c2c729cc6d710d4
ripperd-2882 <ripperd2882@gmail.com>
2026-06-07 02:28:40 +0530

    Implement filter functionality in Filter component to update cabin capacity

--- 15-the-wild-oasis-website/app/_components/Filter.js
+++ 15-the-wild-oasis-website/app/_components/Filter.js
@@ -1,7 +1,18 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 function Filter() {
-  function handleFilter(filter) {}
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter); //This sets the path internally but does not writes in address bar
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }
+
   return (
     <div className="border border-primary-800 flex">
       <button
