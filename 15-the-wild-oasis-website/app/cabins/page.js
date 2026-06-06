70f7599f88c3fdb3415db882bf7443c7d8a93318
ripperd-2882 <ripperd2882@gmail.com>
2026-06-06 17:38:42 +0530

    Add noStore call in CabinList and clean up imports in page.js

--- 15-the-wild-oasis-website/app/cabins/page.js
+++ 15-the-wild-oasis-website/app/cabins/page.js
@@ -1,8 +1,8 @@
 import { Suspense } from "react";
-import CabinCard from "../_components/CabinCard";
 import CabinList from "../_components/CabinList";
-import { getCabins } from "../_lib/data-service";
 import Spinner from "../_components/Spinner";
+
+// export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
