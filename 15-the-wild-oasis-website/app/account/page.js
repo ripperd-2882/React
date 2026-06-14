4e98742ca6475e44957d5246e46c222c3bb169e3
ripperd-2882 <ripperd2882@gmail.com>
2026-06-14 14:44:27 +0530

    Implement sign-in functionality with Google and update user greeting

--- 15-the-wild-oasis-website/app/account/page.js
+++ 15-the-wild-oasis-website/app/account/page.js
@@ -2,10 +2,11 @@ import { auth } from "../_lib/auth";
 
 export default async function Page() {
   const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {session?.user?.name}
+      Welcome, {firstName}
     </h2>
   );
 }
