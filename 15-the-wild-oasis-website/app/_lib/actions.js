271ea9b4ddeda64ded17cef95d7af8f7035a7c64
ripperd-2882 <ripperd2882@gmail.com>
2026-06-14 14:49:57 +0530

    Add sign-out functionality with SignOutButton component and update actions

--- 15-the-wild-oasis-website/app/_lib/actions.js
+++ 15-the-wild-oasis-website/app/_lib/actions.js
@@ -1,7 +1,11 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction() {
   await signIn("google", { redirectedTo: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
