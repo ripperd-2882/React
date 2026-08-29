0d7b7f75c61d088659c00f110fea1dff13582848
ripperd-2882 <ripperd2882@gmail.com>
2026-08-30 02:31:15 +0530

    Add thank you page for reservation confirmation and redirect after booking

--- 15-the-wild-oasis-website/app/_lib/actions.js
+++ 15-the-wild-oasis-website/app/_lib/actions.js
@@ -49,6 +49,8 @@ export async function createBooking(bookingData, formData) {
   if (error) throw new Error("Booking could not be created");
 
   revalidatePath(`/cabins/${bookingData.cabinId}`);
+
+  redirect(`/cabins/thankyou`);
 }
 
 export async function deleteBooking(bookingId) {
